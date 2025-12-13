36b42a1c2aa88cfdc0f131eeed8e913d40ac27b5
Eyosiyas <eyosiyas679@gmail.com>
2025-12-13 19:53:36 +0000

    minimized

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,10 @@
 import 'package:flutter/material.dart';
+// import 'package:helloworld/splash_screen.dart';
+import 'package:helloworld/home_page.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset('assets/images/cafe-template.jpg', width: 300),
-              Text(
-                'CAFETERIA LEDGER',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xffffa701),
-                ),
-              ),
-              Text('APP',
-                  style: TextStyle(
-                    color: Color(0xffffa701),
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(height: 100),
-              Text('coming soon...',
-                  style: TextStyle(
-                    color: Color(0xffaba2a2),
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ],
-          ),
-        ),
-      ),
-    ),
+//    const SplashScreen(),
+    const HomePage(),
   );
 }
